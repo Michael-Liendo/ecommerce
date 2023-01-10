598c04a447fa361558b5246be688d1338ce71bc6
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-01-09 22:40:00 -0400

    BREAKING CHANGE: save add new item to the cart

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -9,12 +9,33 @@ export default function ProductCard({ product }) {
     setTimeout(() => setCartAnimated(false), 200);
   }
 
+  function addNewItem(productId) {
+    setCart((prevCart) => {
+      const productIndex = prevCart.findIndex(
+        (product) => product.id === productId
+      );
+      if (productIndex < 0) {
+        return [...prevCart, { ...product, quantity: 1 }];
+      } else {
+        const newProductArr = prevCart;
+        newProductArr[productIndex] = {
+          ...product,
+          quantity: product.quantity + 1,
+        };
+        return newProductArr;
+      }
+    });
+    console.log(cart);
+    CartAnimation();
+  }
+
   return (
     <a key={product.id}>
       <div className="pb-3 transition duration-300 rounded-lg w-64  hover:shadow-xl">
         <Image
           className="rounded-lg w-full h-56 object-cover"
-          src={product.images[0]}
+          src="https://api.lorem.space/image?w=640&h=480&r=400"
+          // src={product.images[0]}
           alt={product.title}
           width="1000"
           height="1000"
@@ -31,10 +52,7 @@ export default function ProductCard({ product }) {
               ${product.price}
             </span>
             <button
-              onClick={() => {
-                setCart([...cart, product]);
-                CartAnimation();
-              }}
+              onClick={() => addNewItem(product.id)}
               type="button"
               className="py-2 px-3 text-xs font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800"
             >
